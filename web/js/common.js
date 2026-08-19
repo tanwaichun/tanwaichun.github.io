a1d2e4c66a1b14ce559d1783bf5d531347d740bf
Chin Yen Leng <yenleng252026@dasein.edu.my>
2026-08-19 13:42:23 +0800

    style: add script to common.js

--- web/js/common.js
+++ web/js/common.js
@@ -1,12 +1,19 @@
 const toggleButton = document.querySelectorAll(".menu-toggle");
+const closeButton = document.querySelector(".menu-close");
 const menu = document.getElementById("menu");
 
-toggleButton.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    // Change icon
-    if (menu.classList.contains("active")) {
-        toggleButton.innerHTML = "&times;"; // × (cross)
-    } else {
-        toggleButton.innerHTML = "&#9776;"; // ☰ (hamburger)
-    }
+toggleButton.forEach((button) => {
+    button.addEventListener("click", () => {
+        menu.classList.toggle("active");
+        // Change icon
+        if (menu.classList.contains("active")) {
+            button.innerHTML = "&#9776;"; // ☰ (hamburger)
+        } else {
+            button.innerHTML = "&#9776;"; // ☰ (hamburger)
+        }
+    });
+});
+
+closeButton.addEventListener("click", () => {
+    menu.classList.remove("active");
 });
